fd656cb739d585cb02810df73c3283637a98d299
haxul <41199611+haxul@users.noreply.github.com>
2019-10-24 23:39:23 +0400

    fix index.js

--- index.js
+++ index.js
@@ -26,15 +26,4 @@ router.post('/register', async ctx => register(ctx));
 router.post('/signin', async ctx => signin(ctx));
 router.post('/addTask', async ctx => addTask(ctx));
 
-router.post('/removeTask', async ctx => {
-  const conn = await pool.getConnection();
-  const inputData = ctx.request.body;
-  const task = inputData.task;
-  const accessToken = ctx.request.header['access-token'];
-  const response = conn.query(
-    'SELECT * FROM `task-manager`.`account` a INNER JOIN `task-manager`.`accessToken` t ON a.id = t.accountId'
-  );
-  if (!response[0].id) ctx.throw(422, 'user not found');
-});
-
 app.listen(3000, () => console.log('server is running '));
